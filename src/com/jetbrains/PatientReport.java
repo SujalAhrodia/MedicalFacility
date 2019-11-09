91b42e4956d56386e5c5b14265b8f57e316137be
Pooja Patel <poojapatel@Poojas-MacBook-Air.local>
2019-11-09 11:33:43 -0500

    Activating missing Links for Treated Patient menu

--- src/com/jetbrains/PatientReport.java
+++ src/com/jetbrains/PatientReport.java
@@ -1,4 +1,15 @@
 package com.jetbrains;
 
+import java.sql.Connection;
+
 public class PatientReport {
+    Integer pid;
+
+    public void displayReport(Connection conn, Integer pid){
+
+    }
+
+    public void acknowledgeReport(Integer pid){
+
+    }
 }
