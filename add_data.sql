5fc1b86dee2c0865ca66d0af9cd49f09194a9f47
Austin Shafer <amshafe2@ncsu.edu>
2019-11-14 16:51:09 -0500

    Updating add_data

--- add_data.sql
+++ add_data.sql
@@ -36,3 +36,38 @@ INSERT INTO Facility
 (fid, fac_name, classification, no_of_departments, capacity)
 VALUES (1, 'Fac2', 'ER', 0, 25);
 
+/* update the circular dependency in a transaction */
+
+INSERT INTO Service_department
+(dept_id, dept_name, director)
+VALUES (0, 'Big dept', 0);
+
+INSERT INTO Staff
+(user_id, designation, hiredate, primary_dept)
+VALUES (0, 'Medical', TO_DATE('17/12/2016', 'DD/MM/YYYY'), 0);
+
+INSERT INTO Body_part
+(code, part_name)
+VALUES ('HED', 'HEAD');
+
+INSERT INTO Symptom_scale
+(scale_name)
+VALUES ('1-10');
+
+INSERT INTO Symptom
+(symptom_name, code, priority, symptom_scale)
+VALUES ('Headache', 'HDCH', 2, '1-10');
+
+INSERT INTO Implies
+(symptom, part)
+VALUES ('HDCH', 'HED');
+
+INSERT INTO Patient
+(user_id, checkin_time_start, checkin_time_end, checkout_time)
+VALUES (1, TO_DATE('17/12/2015', 'DD/MM/YYYY'), TO_DATE('17/12/2015', 'DD/MM/YYYY'), TO_DATE('17/12/2016', 'DD/MM/YYYY'));
+
+INSERT INTO Patient
+(user_id, checkin_time_start, checkin_time_end, checkout_time)
+VALUES (2, TO_DATE('17/12/2015', 'DD/MM/YYYY'), TO_DATE('17/12/2015', 'DD/MM/YYYY'), null);
+
+COMMIT;
